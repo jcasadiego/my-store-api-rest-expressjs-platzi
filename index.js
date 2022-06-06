5401ee54fcd3ecd191c47b33221fe0b7cffd3dc2
jcasadiego <jcasadiego.a@gmail.com>
2022-06-06 11:29:22 -0400

    routing con express

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ app.get('/', (req, res) => {
   res.send('Hola mi server en express');
 })
 
+app.get('/products', (req, res) => {
+  res.json({
+    name:'Producto 1',
+    price: 1000
+  });
+})
+
+app.get('/home', (req, res) => {
+  res.send('Este es mi Home');
+})
+
+app.get('/categories', (req, res) => {
+  res.json([
+    'categoria 1',
+    'caegoria 2'
+  ]);
+})
+
 app.listen(port, () => {
   console.log('Mi port: ' + port);
 })
