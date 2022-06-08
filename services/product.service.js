2ed7dc32a8bfe3ba61860b4d608b537bf9d24cf7
jcasadiego <jcasadiego.a@gmail.com>
2022-06-08 06:39:19 -0400

    capturando errores con middlewares

--- services/product.service.js
+++ services/product.service.js
@@ -1,4 +1,5 @@
 const faker = require('faker');
+const boom = require('@hapi/boom');
 
 class ProductsService {
 
@@ -14,7 +15,8 @@ class ProductsService {
         id: faker.datatype.uuid(),
         name: faker.commerce.productName(),
         price: parseInt(faker.commerce.price(), 10),
-        image: faker.image.imageUrl()
+        image: faker.image.imageUrl(),
+        isBlock: faker.datatype.boolean(),
       });
     };
   }
@@ -33,13 +35,20 @@ class ProductsService {
   }
 
   async findOne(id){
-    return this.products.find(item => item.id === id);
+    const product = this.products.find(item => item.id === id);
+    if(!product){
+      throw boom.notFound('product not found');
+    }
+    if(product.isBlock){
+      throw boom.conflict('product is block');
+    }
+    return product;
   }
 
   async update(id, changes){
     const index = this.products.findIndex(item => item.id === id);
     if(index === -1){
-      throw new Error('product not found')
+      throw boom.notFound('product not found');
     }
     const product = this.products[index];
     this.products[index] = {
@@ -52,7 +61,7 @@ class ProductsService {
   async delete(id){
     const index = this.products.findIndex(item => item.id === id);
     if(index === -1){
-      throw new Error('product not found')
+      throw boom.notFound('product not found');
     }
     this.products.splice(index, 1);
     return { id };
